2c724a8235e5029b60c0ca54d1b4e864f41bc899
Jabox <jasonadams023@gmail.com>
2016-08-10 10:10:53 -0500

    Authenticate user page added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
-#  before_action :authenticate_user_page
-#
-#  private
-#  def authenticate_user_page
-#  	if params[:user_id] != 
-#  end
+  before_action :authenticate_user_page
+
+  private
+  def authenticate_user_page
+  	if params[:user_id] != nil && params[:user_id].to_i != current_user.id
+		flash[:alert] = "You are not signed in as that user!"
+		redirect_to root_path
+	end
+  end
 end
